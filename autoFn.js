2f2adb9d112aaec190b21870bb848f796fda3d26
イざけ <akha3743@gmail.com>
2025-08-22 22:09:51 +0530

    add autodetech dir changes
    add fns to autodetect changes (new files, rename), rename fn names for cleancode

--- autoFn.js
+++ autoFn.js
@@ -2,9 +2,10 @@
 
 const fs = require('node:fs').promises;
 const path = require('node:path');
+const { watch } = require('node:fs/promises');
 const { getTemplate } = require('./template');
 
-async function readAllDirs (parentPath, extensions) {
+async function getAllDirectoryFiles (parentPath, extensions) {
     let filesPath = [];
     try {
         const childrens = await fs.readdir(parentPath);
@@ -14,7 +15,7 @@ async function readAllDirs (parentPath, extensions) {
             const stats = await fs.stat(childrenPath);
 
             if (stats.isDirectory()) {
-                const dirChildren = await readAllDirs(childrenPath, extensions);
+                const dirChildren = await getAllDirectoryFiles(childrenPath, extensions);
                 filesPath = filesPath.concat(dirChildren);
             } else if (stats.isFile()) {
                 if (extensions.includes(path.extname(children))) filesPath.push(childrenPath);
@@ -24,6 +25,27 @@ async function readAllDirs (parentPath, extensions) {
         console.error("Error reading dir: ", err.message);
     }
     return filesPath;
+}
+
+async function getAllDirectories (rootDir) {
+    let folders = [];
+    try {
+        const contents = await fs.readdir(rootDir);
+        for (const item of contents) {
+            const itemPath = path.join(rootDir, item);
+
+            const stats = await fs.stat(itemPath);
+
+            if (stats.isDirectory()) {
+                folders.push(itemPath);
+                const subDir = await getAllDirectories(itemPath);
+                folders = folders.concat(subDir);
+            }
+        }
+    } catch (err) {
+        console.error("Error reading dir: ", err.message);
+    }
+    return folders;
 }
 
 function writeAllFiles (filesPaths) {
@@ -42,8 +64,6 @@ function writeAllFiles (filesPaths) {
             })
             console.log("data written successfully");
 
-
-
         } catch (err) {
             console.error("error checking file: ", err);
         }
@@ -52,18 +72,42 @@ function writeAllFiles (filesPaths) {
     return;
 }
 
-async function autoFunction () {
-
-    const res = await fs.readFile('./conf.json', 'utf8');
-    const json = await JSON.parse(res);
-    const extensions = json.target;
-    const dirPath = json.dir;
-
-    const dirs = await readAllDirs(dirPath, extensions);
+async function autoFunction (dirPath, extensions) {
+    const dirs = await getAllDirectoryFiles(dirPath, extensions);
     writeAllFiles(dirs);
 
     return;
 }
 
-autoFunction();
+const listenDirChanges = async (dirPath, extensions) => {
+    const dirPaths = await getAllDirectories(dirPath); 
+    console.log(dirPaths);
+    dirPaths.forEach(async (d_path) => {
+        try {
+            const watcher = watch(d_path);
+            for await (const event of watcher) {
+                if (['change', 'rename'].includes(event.eventType)) autoFunction(d_path, extensions);
+            }
+        } catch (err) {
+            console.error(`Error watching ${d_path}:`, err.message);
+        }
+    });
+} 
+
+
+(
+    async () => {
+        try {
+            const res = await fs.readFile('./conf.json', 'utf8');
+            const json = await JSON.parse(res);
+            const extensions = json.target;
+            const dirPath = json.dir;
+
+            autoFunction(dirPath, extensions); 
+            await listenDirChanges(dirPath, extensions);
+        } catch(err) {
+            console.error(`init error: ${err.message}`);
+        }
+    }
+)();
 
