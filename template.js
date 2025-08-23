ae00c4950453f096a7aefc8b75177d7e7fce39bd
イざけ <akha3743@gmail.com>
2025-08-23 15:25:44 +0530

    remove bloat
    simplicity over complexity

--- template.js
+++ template.js
@@ -1,24 +1,11 @@
 "use strict";
 
-const fs = require('fs/promises');
-const path = require('node:path');
+const getTemplate = async (extensions, extension, fileName) => {
+    if (!extensions.includes(extension)) throw new Error(`Extension ${extension} ins't supported`);
 
-const getTemplate = async (extension, fileName) => {
-    const configPath = path.join(__dirname, 'conf.json');
-    try {
-        const res = await fs.readFile(configPath, 'utf8');
-        const json = await JSON.parse(res);
+    const fn = getFunction(extension);
+    return fn(fileName);
 
-        const extensions = json.target;
-        if (!extensions.includes(extension)) throw new Error(`Extension ${extension} ins't supported`);
-        
-        const fn = getFunction(extension);
-        return fn(fileName);
-
-    } catch (err) {
-        console.error("Error reading conf.json file: ",err);
-        return " ";
-    }
 }
 
 
